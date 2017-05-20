e38ed069b9a57e04cf5038c72633b7516566bf1c
liyang wang <wangliyang88@gmail.com>
2017-05-20 13:58:19 -0700

    working now

--- client/components/profile.js
+++ client/components/profile.js
@@ -7,6 +7,7 @@ class Profile extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      username: this.props.username,
     }
   }
 
@@ -15,8 +16,9 @@ class Profile extends Component {
     const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
     const dayComponents = [];
     for (let i = 0; i < days.length; i += 1) {
-      dayComponents.push(<Day day={days[i]} />);
+      dayComponents.push(<Day username={this.state.username} day={days[i]} />);
     }
+
     console.log(dayComponents);
     return (
       <div>
