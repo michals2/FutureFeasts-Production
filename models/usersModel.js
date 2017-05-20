d532898574575a5ff295b736eda95c341bc2c4a9
alyssagaudioso <alyssa.gaudioso@gmail.com>
2017-05-19 22:58:01 -0700

    Delete usersModel.js

--- models/usersModel.js
+++
@@ -1,8 +0,0 @@
-// const Sequelize = require('sequelize');
-
-// const sequelize = new Sequelize('futureMeals', 'postgres', 'pw', {
-//     host: 'localhost',
-//     dialect: 'postgres'
-// });
-
-// let User = sequelize
